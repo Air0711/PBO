ef3c5336eb6499bc61693ca562565a964fe1e80b
D-20 <D-20@D-20>
2024-06-03 20:15:30 +0700

    PBO task 1

---
+++ PBO_task_1/src/main/java/com/air/pbo_task_1/Yowhatsupbro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.air.pbo_task_1;
+
+/**
+ *
+ * @author D-20
+ */
+public class Yowhatsupbro {
+    public static void main(String[] args) {
+        System.out.println("Yo whatsup bro");
+    }
+    
+}
